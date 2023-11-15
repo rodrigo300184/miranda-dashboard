c2ddb0dcfc94f98deba741c0d242417f7a75df1c
Rodrigo <rodrigo300184@gmail.com>
2023-11-15 16:55:49 +0100

    message: working on!

--- src/pages/EmployeeUpdate.tsx
+++ src/pages/EmployeeUpdate.tsx
@@ -1,9 +1,23 @@
+import styled from "styled-components";
+import colors from "../styles/colors";
+
+const H1 = styled.h1`
+margin-top:150px;
+  text-align: center;
+  color: ${colors.green};
+`;
+const H3 = styled.h3`
+  text-align: center;
+  color: ${colors.mattBlack};
+`;
+
+
 export const EmployeeUpdate = () => {
 
     return (
         <>
-        <h1>Hello Employee Update</h1>
-        <h3>I'm still working on it</h3>
+        <H1>Hello Employee Update</H1>
+        <H3>I'm still working on it</H3>
         </>
     )
 }
--- src/pages/RoomDetails.tsx
+++ src/pages/RoomDetails.tsx
@@ -1,10 +1,23 @@
+import styled from "styled-components";
+import colors from "../styles/colors";
 
+
+const H1 = styled.h1`
+margin-top:150px;
+  text-align: center;
+  color: ${colors.green};
+`;
+const H3 = styled.h3`
+  text-align: center;
+  color: ${colors.mattBlack};
+`;
 
 export const RoomDetails = () => {
 
     return (
         <>
-        <h1>Hello Room Details</h1>
+        <H1>Hello Room Details</H1>
+        <H3>I'm still working on it</H3>
         </>
     )
 }
