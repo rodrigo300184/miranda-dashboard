ab0422b8b94ee562eca9b24f36240834328048e4
Rodrigo <rodrigo300184@gmail.com>
2024-01-05 19:01:58 +0100

    Added Reviews and swiper  in dashboard

---
+++ src/components/Reviews.tsx
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css/bundle";
+
+const ReviewsContainer = styled.div`
+  margin: 50px;
+  box-shadow: 0px 4px 4px #00000005;
+  border-radius: 20px;
+  background: #ffffff 0% 0% no-repeat padding-box;
+`;
+
+const Title = styled.h1`
+  text-align: left;
+  font: 500 20px Poppins;
+  letter-spacing: 0px;
+  color: #393939;
+  opacity: 1;
+`;
+
+export const Reviews = () => {
+  return (
+    <>
+      <ReviewsContainer>
+        <Title>Latest Review by Customers</Title>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          navigation
+          pagination={true}
+          modules={[Navigation]}
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+        </Swiper>
+      </ReviewsContainer>
+    </>
+  );
+};
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,10 @@
 import { Kpis } from "../components/Kpis";
+import { Reviews } from "../components/Reviews";
 export const Dashboard = () => {
   return (
     <>
       <Kpis />
-   
+      <Reviews />
     </>
   );
 };
