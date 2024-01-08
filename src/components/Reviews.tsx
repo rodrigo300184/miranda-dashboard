e9a6d906077d6dc460b92048d4b0eaf604a8bfaf
Rodrigo <rodrigo300184@gmail.com>
2024-01-08 11:53:42 +0100

    ReviewCard added to Reviews component

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -5,9 +5,13 @@ import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
 import "swiper/css/bundle";
+import { ReviewCard } from "./ReviewCard";
+import { ErrorMessage } from "./ErrorMessage";
+import { Spinner } from "./Spinner";
 
 const ReviewsContainer = styled.div`
   margin: 50px;
+  padding:30px;
   box-shadow: 0px 4px 4px #00000005;
   border-radius: 20px;
   background: #ffffff 0% 0% no-repeat padding-box;
@@ -30,28 +34,34 @@ export const Reviews = () => {
       dispatch(fetchContacts())
     },[dispatch])
   
-    //   const filteredBookingsData = useMemo(() =>{return filterAndOrder(bookingsData, filter, orderBy);
-    // }, [bookingsData, filter, orderBy]);
   return (
     <>
       <ReviewsContainer>
         <Title>Latest Review by Customers</Title>
+        {contactsDataStatus === "rejected" ? (
+        <ErrorMessage />
+      ) : contactsDataStatus === "pending" ? (
+        <Spinner />
+      ) : (
         <Swiper
-          spaceBetween={50}
+          spaceBetween={40}
           slidesPerView={3}
           navigation
           pagination={true}
           modules={[Navigation]}
-        >
-          <SwiperSlide>Slide 1</SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
+        >{contactsData.map((contact) => {
+            return (
+                <>
+                <SwiperSlide><ReviewCard contact={contact} /></SwiperSlide>
+                </>
+            )
+        })}
+          
+          
         </Swiper>
+        
+      )}
+        
       </ReviewsContainer>
     </>
   );
