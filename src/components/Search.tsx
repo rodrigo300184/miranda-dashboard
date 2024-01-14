2c71445e9c4d5751509c261318cc619886621a58
Rodrigo <rodrigo300184@gmail.com>
2024-01-14 21:17:39 +0100

    searc padding corrected for right side

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -22,7 +22,7 @@ const SearchInput = styled.input`
   margin-top: 50px;
   border-radius: 12px;
   border: 2px solid rgb(19, 88, 70);
-  padding-left: 35px; /* Ajusta el padding para dejar espacio para el ícono */
+  padding-right: 35px; /* Ajusta el padding para dejar espacio para el ícono */
 `;
 
 const SearchIcon = styled(FontAwesomeIcon)`
