0f84de7307fcc5259ba153a1c5983398a2ca19d8
Rodrigo <rodrigo300184@gmail.com>
2024-01-14 21:16:07 +0100

    search component magnifier icon added

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,19 @@
 import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import colors from "../styles/colors";
 
 interface SearchProps {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
+const SearchContainer = styled.div`
+  position: relative;
+  display: flex;
+
+`;
+
 const SearchInput = styled.input`
-  justify-item: end;
   font: 500 16px Poppins;
   color: ${colors.green};
   padding: 5px;
@@ -15,8 +22,23 @@ const SearchInput = styled.input`
   margin-top: 50px;
   border-radius: 12px;
   border: 2px solid rgb(19, 88, 70);
+  padding-left: 35px; /* Ajusta el padding para dejar espacio para el ícono */
+`;
+
+const SearchIcon = styled(FontAwesomeIcon)`
+  position: absolute;
+  top: 58%;
+  right: 12px;
+  transform: translateY(-50%);
+  color: ${colors.green};
 `;
 
 export const Search: React.FC<SearchProps> = (props) => {
-  return <SearchInput onChange={props.onChange} />;
+  return (
+    <SearchContainer>
+      <SearchIcon icon={faSearch} />
+      <SearchInput onChange={props.onChange} />
+    </SearchContainer>
+  );
 };
+
