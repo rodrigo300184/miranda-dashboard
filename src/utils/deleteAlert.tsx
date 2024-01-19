7755bdb0cb978eed806797961d4503870af4f52c
Rodrigo <rodrigo300184@gmail.com>
2024-01-19 17:25:15 +0100

    DeleteAlert function to ask before deletion of files

---
+++ src/utils/deleteAlert.tsx
@@ -0,0 +1,28 @@
+import Swal from "sweetalert2";
+
+const deleteAlert = () => {
+  return new Promise((resolve) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "rgb(226, 52, 40)",
+      cancelButtonColor: "#135846",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    });
+  });
+};
+
+export default deleteAlert;
